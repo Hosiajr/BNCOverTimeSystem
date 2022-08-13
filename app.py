808360dd13d35b88bb8bd45e3bd47299284ef072
Hosiajr <74817291+Hosiajr@users.noreply.github.com>
2022-08-13 10:10:49 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app.secret_key = "hosia12345"
 @app.route("/home")
 def index():
     return render_template("index.html") 
+
+@app.route("/register")
+def register():
+    return render_template("register.html") 
